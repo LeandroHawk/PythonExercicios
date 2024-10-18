75e07ea493d3f87ed629fa474bc50af4e5acae81
Leandro Felix <107886470+LeandroHawk@users.noreply.github.com>
2024-10-18 19:11:13 -0300

    Add files via upload

---
+++ Mercado.py
@@ -0,0 +1,6 @@
+# Solicitar as informações do produto
+nome_produto = input("Digite o nome do produto: ")
+preco_produto = float(input("Digite o preço do produto em R$: "))
+
+# Exibir as informações do produto
+print(f"Produto: {nome_produto}, Preço: R$ {preco_produto:.2f}")
